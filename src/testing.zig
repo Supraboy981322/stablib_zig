cfb5d75524a4d1a4bf178f9cb3800f80e61c9abb
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-06-30 19:00:38 -0500

    more helpers

--- src/testing.zig
+++ src/testing.zig
@@ -10,10 +10,15 @@ pub fn expectMany(pass_conditions:[]const bool) !void {
     inline for (pass_conditions) |success| try expect(success);
 }
 
-pub fn expectEqualSlices(comptime T:type, one:[]const T, two:[]const T) !void {
+pub fn expectEqlSlices(comptime T:type, one:[]const T, two:[]const T) !void {
     try expect(mem.eql(T, one, two));
 }
-pub fn expectEqualMatrices(comptime T:type, one:Matrix(T), two:Matrix(T)) !void {
+pub fn expectEqlMatrices(comptime T:type, one:Matrix(T), two:Matrix(T)) !void {
     try expect(one.len == two.len);
-    for (0..one.len) |i| try expectEqualSlices(T, one[i], two[i]);
+    for (0..one.len) |i| try expectEqlSlices(T, one[i], two[i]);
+}
+pub fn expectManyEqlSlices(comptime T:type, slices:Matrix(T)) !void {
+    if (slices.len < 2) return;
+    const one = slices[0];
+    for (slices[1..]) |slice| try expectEqlSlices(T, one, slice);
 }

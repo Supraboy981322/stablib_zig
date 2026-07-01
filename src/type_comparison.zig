cd3d463b3b6ffb813698ff70d1a5514f864144a7
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-06-30 19:02:30 -0500

    TypeEnum ; comparing typeInfo

---
+++ src/type_comparison.zig
@@ -0,0 +1,75 @@
+const general = @import("general.zig");
+const mem = @import("mem.zig");
+const meta = @import("meta.zig");
+const types = @import("types.zig");
+
+const builtin = types.builtin;
+const comptimeAssert = general.comptimeAssert;
+const activeTag = meta.activeTag;
+const allTrue = general.allTrue;
+
+pub const strict = struct {
+
+    // NOTE:
+    //  - you can provide:
+    //      a type and a value (or vice versa),
+    //      two types,
+    //      two values
+    //  - this only compares the types (meaning the value is ignored)
+    // TODO:
+    //  - a 'meta.walkType' module with a 'eql' generic function to
+    //      walk stuff like structs and unions, with 'eql' just to
+    //        comparing the value of each field (handling things like slices)
+    //          this would allow this function to just be:
+    //      comptime {
+    //          const T1, const T2 = .{
+    //              if (@TypeOf(one) == type) one else @TypeOf(one),
+    //              if (@TypeOf(two) == type) two else @TypeOf(two),
+    //          };
+    //          const o, const t = .{ @typeInfo(T1), @typeInfo(T2) };
+    //          return meta.compare.eql(@typeInfo(T1, T2);
+    //      }
+    pub fn enumerations(one:anytype, two:anytype) bool {
+        comptime {
+            const T1, const T2 = .{
+                if (@TypeOf(one) == type) one else @TypeOf(one),
+                if (@TypeOf(two) == type) two else @TypeOf(two),
+            };
+            const o, const t = .{ @typeInfo(T1), @typeInfo(T2) };
+            comptimeAssert(o == .@"enum" and t == .@"enum");
+            const e1, const e2 = .{ o.@"enum", t.@"enum" };
+            return allTrue(&.{
+                e1.tag_type == e2.tag_type,
+                e1.is_exhaustive == e2.is_exhaustive,
+                blk: {
+                    const f1, const f2 = .{ e1.fields, e2.fields };
+                    if (f1.len != f2.len) break :blk false;
+                    for (0..f1.len) |i| if (!allTrue(&.{
+                        mem.eql(u8, f1[i].name, f2[i].name),
+                        f1[i].value == f2[i].value,
+                    }))
+                        return false;
+                    break :blk true;
+                },
+                blk: {
+                    const f1, const f2 = .{ e1.decls, e2.decls };
+                    if (f1.len != f2.len) break :blk false;
+                    for (0..f1.len) |i|
+                        if (!mem.eql(u8, f1[i].name, f2[i].name))
+                            return false;
+                    break :blk true;
+                },
+            });
+        }
+        unreachable;
+    }
+};
+
+pub const loose = struct {
+    pub fn matches(comptime T:type, comptime what:builtin.Type) bool {
+        activeTag(@typeInfo(T)) == activeTag(what);
+    }
+    pub fn is(comptime T:type, comptime what:builtin.TypeEnum) bool {
+        return activeTag(@typeInfo(T)) == what;
+    }
+};
--- src/types.zig
+++ src/types.zig
@@ -1,6 +1,13 @@
 const meta = @import("meta.zig");
+const general = @import("general.zig");
+
+const comptimeAssert = general.comptimeAssert;
+const comptimeAssertMany = general.comptimeAssertMany;
+const activeTag = meta.activeTag;
+
 pub const builtin = struct {
     pub const Type = @TypeOf(@typeInfo(u8));
+    pub const TypeEnum = meta.Tag(Type);
 };
 
 pub const HashMap = @import("hashmap.zig").HashMap;
@@ -15,7 +22,49 @@ pub fn Matrix(comptime T:type) type {
     return []const []const T;
 }
 
-// TODO: not enough for some types
-pub fn is(comptime T:type, comptime what:builtin.Type) bool {
-    meta.activeTag(@typeInfo(T)) == meta.activeTag(what);
-}
+pub const compare = @import("type_comparison.zig");
+pub const is = compare.loose.is;
+
+pub const atomic = struct {
+    //ripped straight from stdlib
+    pub const AtomicOrder = enum {
+        unordered,
+        monotonic,
+        acquire,
+        release,
+        acq_rel,
+        seq_cst,
+    };
+    //idotically, Zig's atomic builtins use a param type only
+    //  available from stdlib this verifies that it still matches,
+    //    because I do not trust it to even remain defined in the
+    //      standard library
+    comptime {
+        const std = @import("std");
+        const Real = std.builtin.AtomicOrder;
+        comptimeAssert(
+            compare.strict.enumerations(Real, AtomicOrder)
+        );
+    }
+
+    pub const Mutex = struct {
+        lock_value:atomic.Value(bool),
+        pub const init:Mutex = .{ .thing = .init(false) };
+        // TODO
+    };
+    pub fn Value(comptime T:type) type {
+        return struct {
+            raw:T,
+
+            const Self = @This();
+
+            pub fn init(v:T) Self {
+                return .{ .raw = v };
+            }
+
+            pub fn set(self:*Self, value:T) void {
+                @atomicStore(T, &self.raw, value, .seq_cst);
+            }
+        };
+    }
+};

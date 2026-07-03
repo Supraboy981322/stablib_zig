7c3c925db07a86e3327d8e25ce42e4e0c0d270ea
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-07-02 20:09:32 -0500

    add mutex to io

--- src/io.zig
+++ src/io.zig
@@ -1,42 +1,47 @@
 const module = @import("module.zig");
 
 const fmt = module.fmt;
+const time = module.time;
 const posix = module.posix;
-const Mutex = module.types.atomic.Mutex;
+const types = module.types;
 
+const Mutex = types.atomic.Mutex;
 pub const Fd = posix.Fd;
 const bufPrint = fmt.bufPrint;
+const Atomic = types.atomic.Value;
 
 const io = @This();
 
+var mutex:Mutex = .{};
+
+var disable_mutex:Atomic(bool, .seq_cst) = .init(false);
+
+pub const NamedOpts = struct {
+    bypass_mutex:bool = false,
+};
+fn lock() void {
+    if (!disable_mutex.get({})) mutex.lock();
+}
+fn unlock() void {
+    if (!disable_mutex.get({})) mutex.unlock();
+}
+
+pub fn disableMutex() error{AlreadyDisabled}!void {
+    if (disable_mutex.get({})) return error.AlreadyDisabled;
+    disable_mutex = true;
+}
+
 // TODO:
 //  - this is super inefficient
-//  - this *will* fail if formatted string would be larger than 
+//  - this *will* fail if formatted string would be larger than (length of msg) + (size of args)
 pub fn print(fd:Fd, comptime msg:[]const u8, args:anytype) !void {
+    lock(); defer unlock();
     var buf:[msg.len + @sizeOf(@TypeOf(args))]u8 = undefined;
     const str = bufPrint(&buf, msg, args) catch unreachable; // TODO: refactor print
     try posix.print(fd, str);
 }
 
-pub const WithMutex = struct {
-    mut:Mutex,
-
-    pub fn init(mut:*Mutex) WithMutex {
-        return .{ .mut = mut.* };
-    }
-
-    pub fn print(self:*WithMutex, fd:Fd, comptime msg:[]const u8, args:anytype) !void {
-        self.mut.lock();
-        defer self.mut.unlock();
-        try io.print(fd, msg, args);
-    }
-
-    // TODO: async of some kind to test this
-};
-
-test WithMutex {
-    _ = WithMutex;
-}
-
-test "basic" {
+pub fn write(fd:Fd, bytes:[]const u8) !void {
+    lock(); defer unlock();
+    try posix.writeAll(fd.value(), bytes);
 }

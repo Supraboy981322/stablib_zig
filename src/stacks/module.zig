e32d931e534b20f9772adb33edb9beb96b7ceb73
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-07-08 23:50:25 -0500

    BitStack and ByteStacl

--- src/stacks/module.zig
+++ src/stacks/module.zig
@@ -1,6 +1,13 @@
 pub const BasicStack = @import("BasicStack.zig").BasicStack;
 
 
+pub fn BasicByteStack(comptime size:usize) type {
+    return BasicStack(u8, size);
+}
+pub fn BasicBitStack(comptime size:usize) type {
+    return BasicStack(u1, size);
+}
+
 
 test "stacks" {
     _ = BasicStack;
